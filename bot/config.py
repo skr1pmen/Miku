d65c312e369b6a28aee279a09fdd4b2834986e3a
Skrip men <kiberkot.v.roymenge@gmail.com>
2021-09-30 16:11:06 +0400

    Version 4.3

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,8 @@
 settings={
     'token': 'NTUzNTM4ODczODI1Njg5NjAw.XIJRPw.Rzeq0khZBCoXGdPAa9Wufv1PV30',
-    'version': '4.2.1',
+    'version': '4.3',
     'prefix': '.',
     'guild_ids': '547094388539392000',
-    'debug': True,
+    'debug': False,
     'versionDebug': 'Тестирование обновления'
 }
