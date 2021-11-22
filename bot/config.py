727e76da08c1e9efb6a1303559161434d1be3c98
Skrip men <kiberkot.v.roymenge@gmail.com>
2021-11-22 22:36:50 +0400

    Version 4.4.2

--- bot/config.py
+++ bot/config.py
@@ -1,6 +1,6 @@
 settings={
     'token': 'NTUzNTM4ODczODI1Njg5NjAw.XIJRPw.Rzeq0khZBCoXGdPAa9Wufv1PV30',
-    'version': '4.4.1',
+    'version': '4.4.2',
     'prefix': '.',
     'guild_ids': '547094388539392000',
     'debug': False,
