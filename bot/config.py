0300fb912f1144f6aed283158b02bb6a9b52b0c0
skr1pmen <48603232+skr1pmen@users.noreply.github.com>
2021-12-14 13:07:56 +0300

    Version 4.5.1 (Patch 1)

--- bot/config.py
+++ bot/config.py
@@ -3,6 +3,6 @@ settings={
     'version': '4.5.1',
     'prefix': '.',
     'guild_ids': '547094388539392000',
-    'debug': True,
+    'debug': False,
     'versionDebug': 'Тестирование обновления'
 }
