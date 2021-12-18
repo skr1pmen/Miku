1752537581876d4250433d3a0737e88a9b61a795
Skrip men <kiberkot.v.roymenge@gmail.com>
2021-12-18 11:39:17 +0300

    Version 4.5.2

---
+++ bot/cogs/Logs.py
@@ -0,0 +1,61 @@
+from discord.ext import commands
+import discord
+from config import settings
+
+
+class Logs(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        msg = f"{member.name} присоединился к серверу."
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(settings['logChannel'])
+        await channel.send(msg)
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        msg = f"{member.name} покинул сервер."
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(settings['logChannel'])
+        await channel.send(msg)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        msg = discord.Embed(title=f"{before.author.name} изменил сообщение.")
+        msg.add_field(name=f"Было:", value=f"``{before.content}``", inline = False)
+        msg.add_field(name=f"Стало:", value=f"``{after.content}``", inline = False)
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(settings['logChannel'])
+        await channel.send(embed = msg)
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        msg = discord.Embed(title=f"{message.author.name} удалил сообщение.", description=f"{message.content}")
+        await self.bot.wait_until_ready()
+        channel = self.bot.get_channel(settings['logChannel'])
+        await channel.send(embed = msg)
+
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+        if before.channel is None:
+            msg = f"{member.display_name} присоединился к каналу {after.channel.mention}"
+            await self.bot.wait_until_ready()
+            channel = self.bot.get_channel(settings['logChannel'])
+            await channel.send(msg)
+        elif after.channel is None:
+            msg = f"{member.display_name} вышел из канала {before.channel.mention}"
+            await self.bot.wait_until_ready()
+            channel = self.bot.get_channel(settings['logChannel'])
+            await channel.send(msg)
+        elif before.channel != after.channel:
+            msg = f"{member.display_name} перешёл из канала {before.channel.mention} в канал {after.channel.mention}"
+            await self.bot.wait_until_ready()
+            channel = self.bot.get_channel(settings['logChannel'])
+            await channel.send(msg)
+
+
+def setup(bot):
+    bot.add_cog(Logs(bot))
--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,9 @@
 settings={
     'token': 'NTUzNTM4ODczODI1Njg5NjAw.XIJRPw.Rzeq0khZBCoXGdPAa9Wufv1PV30',
-    'version': '4.5.1',
+    'version': '4.5.2',
     'prefix': '.',
     'guild_ids': '547094388539392000',
-    'debug': True,
-    'versionDebug': 'Тестирование обновления'
+    'debug': False,
+    'versionDebug': 'Тестирование обновления',
+    'logChannel': 921364597888413766
 }
